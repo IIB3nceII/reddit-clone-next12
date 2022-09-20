0edab0408771dbd914105f67668574377f35b610
Bence Papp <bencepapp1101@gmail.com>
2022-09-20 15:43:26 +0200

    header created

---
+++ common/Divider/Divider.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./Divider.module.scss";
+
+const Divider = () => {
+  return <hr />;
+};
+
+export default Divider;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,50 @@
 import Image from "next/image";
 import React from "react";
 import s from "./Header.module.scss";
+import {
+  HomeIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  BellIcon,
+  ChatBubbleOvalLeftIcon,
+  GlobeAmericasIcon,
+  PlusIcon,
+  PhoneIcon,
+  VideoCameraIcon,
+  SparklesIcon,
+} from "@heroicons/react/24/solid";
+import { Divider } from "../../common";
 
 const Header = () => {
   return (
-    <div className={s.container}>
+    <header className={s.container}>
       <div className={s.imageContainer}>
         <Image objectFit="contain" src="https://logos-world.net/wp-content/uploads/2020/10/Reddit-Logo.png" layout="fill" />
       </div>
-    </div>
+
+      <div className={s.options}>
+        <HomeIcon className={s.icon} />
+        <p>Home</p>
+        <ChevronDownIcon className={s.icon} />
+      </div>
+
+      <form>
+        <MagnifyingGlassIcon className={s.icon} />
+        <input type="text" placeholder="Search Reddit..." />
+        <button type="submit" hidden></button>
+      </form>
+
+      <nav>
+        <SparklesIcon className={s.icon} />
+        <GlobeAmericasIcon className={s.icon} />
+        <VideoCameraIcon className={s.icon} />
+        <Divider />
+        <ChatBubbleOvalLeftIcon className={s.icon} />
+        <BellIcon className={s.icon} />
+        <PlusIcon className={s.icon} />
+        <PhoneIcon className={s.icon} />
+      </nav>
+    </header>
   );
 };
 
